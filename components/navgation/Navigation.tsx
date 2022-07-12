855936ae763ba0bf39cb1d2642060b6b2b093e32
Andrew <legendarysuwe@gmail.com>
2022-07-12 21:55:58 +0300

    🆕 Navigation added

---
+++ components/navgation/Navigation.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const menu = [
+  { title: 'Главная', path: '/' },
+  { title: 'Правила', path: '/rules' },
+]
+
+const Navigation = ({ children }: any) => {
+  const router = useRouter()
+
+  return (
+    <>
+      <header className="fixed w-full z-100 transition border-b-2 border-transparent text-gray-50 font-manrope font-bold">
+        <div className="container mx-auto px-3">
+          <div className="flex justify-between items-center py-4 lg:justify-start lg:space-x-10 select-none">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <Link href="/">
+                <a className="focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 rounded-md transition hover:opacity-80 overflow-hidden">
+                  <img src="/logo.png" className="h-10 w-auto" />
+                </a>
+              </Link>
+            </div>
+            <nav className="flex space-x-4 xl:space-x-8">
+              {menu.map((item, index) => {
+                return (
+                  <Link key={index} href={item.path}>
+                    <a
+                      className={`cursor-pointer ${
+                        router.pathname === item.path
+                          ? 'text-red-500 after:bg-red-500 after:block after:w-full after:rounded-lg after:h-[3px] after:-mb-4 after:mt-[14px]'
+                          : 'hover:text-red-500 transition-colors'
+                      }`}>
+                      {item.title}
+                    </a>
+                  </Link>
+                )
+              })}
+            </nav>
+          </div>
+        </div>
+      </header>
+      {children}
+    </>
+  )
+}
+
+export default Navigation
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navigation from '../components/navgation/Navigation'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="bg-neutral-900 text-neutral-50 selection:bg-red-500 selection:text-white antialiased">
+        <Navigation>
+          <Component {...pageProps} />
+        </Navigation>
+      </div>
+    </>
+  )
 }
 
 export default MyApp
