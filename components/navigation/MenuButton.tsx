4b4da0a78732124bc539ce4bf62556c5a611f3ef
Andrew <legendarysuwe@gmail.com>
2022-07-17 20:55:43 +0300

    🔥 Fixed z-index of next/Image component

--- components/navigation/MenuButton.tsx
+++ components/navigation/MenuButton.tsx
@@ -11,62 +11,64 @@ import {
 
 const MenuButton = () => {
   return (
-    <div className="md:hidden ">
-      <Menu>
-        <Menu.Button className="p-2 bg-red-500 rounded-md">
-          <MenuIcon className="h-5 w-5 text-white" />
-        </Menu.Button>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100 origin-top-right"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75 origin-top-right"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95">
-          <Menu.Items className="absolute right-0 left-0 m-2 mt-4 p-2 rounded-lg flex gap-2 flex-col bg-neutral-900 border-2 border-neutral-700 ">
-            <Menu.Item>
-              {({ active }) => (
-                <div className="flex items-center gap-1 flex-col py-4">
-                  <MapIcon className="h-8 w-8" />
-                  <h1 className="font-manrope text-xl font-bold">Меню</h1>
-                </div>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/">
-                  <a className="flex items-center bg-neutral-800 rounded-md p-2">
-                    <HomeIcon className="h-5 w-5 mr-2" />
-                    Главная
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/rules">
-                  <a className="flex items-center bg-neutral-800 rounded-md p-2">
-                    <BookOpenIcon className="h-5 w-5 mr-2" />
-                    Правила
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/emotes">
-                  <a className="flex items-center bg-neutral-800 rounded-md p-2">
-                    <EmojiHappyIcon className="h-5 w-5 mr-2" />
-                    Эмоуты
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </div>
+    <>
+      <div className="md:hidden">
+        <Menu>
+          <Menu.Button className="p-2 bg-red-500 rounded-md">
+            <MenuIcon className="h-5 w-5 text-white" />
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100 origin-top-right"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75 origin-top-right"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95">
+            <Menu.Items className="absolute right-0 left-0 m-2 mt-4 p-2 rounded-lg flex gap-2 flex-col bg-neutral-900 border-2 border-neutral-700 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <div className="flex items-center gap-1 flex-col py-4">
+                    <MapIcon className="h-8 w-8" />
+                    <h1 className="font-manrope text-xl font-bold">Меню</h1>
+                  </div>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link href="/">
+                    <a className="flex items-center bg-neutral-800 rounded-md p-2">
+                      <HomeIcon className="h-5 w-5 mr-2" />
+                      Главная
+                    </a>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link href="/rules">
+                    <a className="flex items-center bg-neutral-800 rounded-md p-2">
+                      <BookOpenIcon className="h-5 w-5 mr-2" />
+                      Правила
+                    </a>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link href="/emotes">
+                    <a className="flex items-center bg-neutral-800 rounded-md p-2">
+                      <EmojiHappyIcon className="h-5 w-5 mr-2" />
+                      Эмоуты
+                    </a>
+                  </Link>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
+    </>
   )
 }
 
