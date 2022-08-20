3c996bb4c2a1e11581571d026355186390a18b99
Andrew <legendarysuwe@gmail.com>
2022-08-20 23:27:02 +0300

    Updated MenuButton

--- components/navigation/MenuButton.tsx
+++ components/navigation/MenuButton.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, forwardRef } from 'react'
 import Link from 'next/link'
 import { Menu, Transition } from '@headlessui/react'
 import { MenuIcon, MapIcon, HomeIcon } from '@heroicons/react/solid'
@@ -16,6 +16,17 @@ const menu = [
     icon: <CollectionIcon className="h-5 w-5 mr-2" />,
   },
 ]
+
+const MyLink: any = forwardRef((props, ref) => {
+  let { href, children, ...rest }: any = props
+  return (
+    <Link href={href}>
+      <a ref={ref} {...rest}>
+        {children}
+      </a>
+    </Link>
+  )
+})
 
 const MenuButton = () => {
   return (
@@ -35,23 +46,19 @@ const MenuButton = () => {
             leaveTo="transform opacity-0 scale-95">
             <Menu.Items className="absolute shadow-lg right-0 left-0 m-2 mt-4 p-2 rounded-lg flex gap-2 flex-col bg-neutral-900 border-2 border-neutral-700 ">
               <Menu.Item>
-                {({ active }) => (
-                  <div className="flex items-center gap-1 flex-col py-4">
-                    <MapIcon className="h-8 w-8" />
-                    <h1 className="font-manrope text-xl font-bold">Меню</h1>
-                  </div>
-                )}
+                <div className="flex items-center gap-1 flex-col py-4">
+                  <MapIcon className="h-8 w-8" />
+                  <h1 className="font-manrope text-xl font-bold">Меню</h1>
+                </div>
               </Menu.Item>
               {menu.map((item, index) => (
                 <Menu.Item key={index}>
-                  {({ active }) => (
-                    <Link href={item.path}>
-                      <a className="flex items-center bg-neutral-800 rounded-md p-2">
-                        {item.icon}
-                        {item.title}
-                      </a>
-                    </Link>
-                  )}
+                  <MyLink href={item.path}>
+                    <a className="flex items-center bg-neutral-800 rounded-md p-2">
+                      {item.icon}
+                      {item.title}
+                    </a>
+                  </MyLink>
                 </Menu.Item>
               ))}
               <Menu.Item>
