8647cdb83d1c6dccb960dc2ad51d15e66bc4cbb3
Andrew <74493360+pe3ep@users.noreply.github.com>
2023-02-16 01:17:09 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -23,6 +23,11 @@ module.exports = {
         destination: 'https://www.donationalerts.com/r/pe3ep',
         permanent: false,
       },
+      {
+        source: '/school',
+        destination: 'https://docs.google.com/presentation/d/1Tg-UwFQkpvazF40xU_PaD7RD23Dltd65B3RpkvxdItI/edit?usp=sharing',
+        permanent: false,
+      },
     ]
   },
 }
